96f208c3f36b895cb5c492efbbcc04ffa7e63521
Sunny Gleason <sunny.gleason@gmail.com>
2018-11-26 00:30:01 +0000

    initial version

---
+++ precache-manifest.8a3fe9d4acc0816a6f566fc296a51fbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d8b42cc84f743fbc78dc",
+    "url": "/static/js/main.d8b42cc8.chunk.js"
+  },
+  {
+    "revision": "081d5589473d516c1cea",
+    "url": "/static/js/1.081d5589.chunk.js"
+  },
+  {
+    "revision": "d8b42cc84f743fbc78dc",
+    "url": "/static/css/main.ba2f4e61.chunk.css"
+  },
+  {
+    "revision": "16cd6a12ba6b98be2e506e2e7e5d6818",
+    "url": "/index.html"
+  }
+];
